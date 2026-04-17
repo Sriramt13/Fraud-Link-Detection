545cb41460dd92c47fd03cf4a80bfd5f412abc49
Sriramt13 <sriramtejapinnamireddy@gmail.com>
2026-04-17 12:18:01 +0530

    Use VITE_API_URL for frontend API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: `${BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json"
   }
